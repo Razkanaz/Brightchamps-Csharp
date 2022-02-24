a41c73053ca2c5873b6f5a69e65665e2309a2c50
Razkanaz <74030846+Razkanaz@users.noreply.github.com>
2022-02-24 15:25:59 +0700

    Add files via upload

---
+++ Project #7/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class program {
+    public static void Main(string[] args) {
+        int age, height;
+
+        age = askint("Hold on there kiddo, you've gotta be at least 13 years old to get on this ride \nhow old are you: ");
+
+        if (age <= 11)
+        {
+            Console.WriteLine("sorry kid, you're not old enough to ride");
+            Console.ReadKey();
+            return;
+        }
+
+        if (age == 12){
+            height = askint("hmmm technically you're not old enough, but i'll give you a pass, as long as you're tall enough \n what's your height (in cm): ");
+        } else if (age <= 17)
+        {
+            height = askint("alright, how tall are you (in cm): ");
+        } else
+        {
+            Console.WriteLine("come on in sir");
+            Console.ReadKey();
+            return;
+        }
+
+        if (height >= 160)
+        {
+            Console.WriteLine("come one in");
+        } else
+        {
+            Console.WriteLine("sorry kid, you're not tall enough to ride");
+        }
+        Console.ReadKey();
+    }
+
+    public static int askint (string text) {   
+        Console.Write(text);
+        string v = Console.ReadLine();
+        while (int.TryParse(v,out _) == false)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("please enter a number");
+            Console.Write(text);
+            v = Console.ReadLine();
+        }
+        return Convert.ToInt32(v);
+    }
+
+}
---
+++ Project #8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class program {
+    public static void Main(string[] args) {
+	
+        while (true)
+        {
+            string input = (Console.ReadLine()).ToUpper();
+            string output= "";
+            string temp;
+            for (int i = 0; i < input.Length; i++)
+            {
+                temp = Convert.ToString(input[i]);
+                if (temp == " ")
+                {
+                    output = $"{output}:blank:";
+                } else if (temp == "?")
+                {
+                    output = $"{output}:QuestionMark:";
+                } else
+                {
+                    output = $"{output}:{temp}{temp}:";
+                }
+            }
+
+            Console.WriteLine(output);
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
