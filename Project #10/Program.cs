8d7557e166fae9b38b80b8adef8f0addd428f7a3
Razkanaz <74030846+Razkanaz@users.noreply.github.com>
2022-03-03 15:23:44 +0700

    Add files via upload

---
+++ Project #10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class program {
+    public static void Main(string[] args) {
+        int n = 0, sum = 0;
+        Console.Write("write a 2 or more digit number: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        if ((Convert.ToString(n)).Length < 2 )
+        {
+            Console.WriteLine("i said MORE THAN 2 YOU BLOODY IDIOT");
+            Console.ReadKey();
+            return;
+        }
+
+        while (n != 0)
+        {
+            sum += n % 10;
+            n = n/10;
+        }
+
+        Console.WriteLine($"the sum of all the digits are {sum}");
+        Console.ReadKey();
+    }
+}
---
+++ Project #9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class program {
+    public static void Main(string[] args) {
+	
+    Console.Write("write a sentence: ");
+    string input = Console.ReadLine();
+    string[] words = input.Split(new[] {" "}, StringSplitOptions.None);
+
+    if (words.Length < 2) {
+        Console.WriteLine("you must enter more than 1 word");
+        Console.ReadKey();
+        return;
+    }
+
+    int letterlenght = 0;
+    string output = "";
+    foreach (string sets in words)
+    {
+        if (sets.Length > letterlenght)
+        {
+            letterlenght = sets.Length;
+            output = sets;
+        }
+    }
+    
+    Console.WriteLine($"the longest word in that sentence is {output}");
+    Console.ReadKey();
+    }
+}
