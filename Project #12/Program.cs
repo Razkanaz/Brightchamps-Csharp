ba06273a4f1a768e740dbd382745e35789ca76f3
Razkanaz <74030846+Razkanaz@users.noreply.github.com>
2022-03-24 15:23:01 +0700

    Add files via upload

---
+++ Project #12/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class program {
+    public static void Main(string[] args) {
+        string a = Console.ReadLine();
+        FileInfo f = new FileInfo(a);
+        Console.WriteLine(f.Length.ToString());
+        Console.ReadKey();
+    }
+}
