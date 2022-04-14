06dc9725ef285f01f5f2539f67e2f5764b6c4fd1
Razkanaz <74030846+Razkanaz@users.noreply.github.com>
2022-04-14 15:24:35 +0700

    Add files via upload

---
+++ Project #15/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class program {
+    public static void Main(string[] args) {
+        DateTime DT1 = new DateTime(2019, 3, 8, 10, 30, 50);
+        Console.WriteLine(DT1.ToString());
+        Console.WriteLine(DT1.Date.ToString("d"));
+        Console.WriteLine(DT1.ToString("g"));
+        Console.WriteLine(DT1.ToString("yyyy:MM:dd HH:mm"));
+
+        Console.ReadKey();
+
+    }
+}
